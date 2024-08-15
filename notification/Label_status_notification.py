6b5f37c629e22497a98e97abaaf54bb1f140c6c2
鸭鸭「カモ」 <Yzcbs123@163.com>
2024-08-16 07:01:22 +0800

    fix🔨: fixed crashes caused by network problems.

--- notification/Label_status_notification.py
+++ notification/Label_status_notification.py
@@ -39,16 +39,22 @@ while True:
 IGNORE_LABELS = {'Retry-1', 'Validation-Retry'}
 
 def check_repo_exists():
-    response = requests.get(
-        f'https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}'
-    )
-    return response.status_code == 404
+    try:
+        response = requests.get(
+            f'https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}'
+        )
+        return response.status_code == 404
+    except Exception:
+        return False
 
 def check_pr_exists():
-    response = requests.get(
-        f'https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/issues/{PR_NUMBER}'
-    )
-    return response.status_code == 404
+    try:
+        response = requests.get(
+            f'https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/issues/{PR_NUMBER}'
+        )
+        return response.status_code == 404
+    except Exception:
+        return False
 
 def check_pr_labels():
     # --- 无法应对网络错误 ---
@@ -57,40 +63,41 @@ def check_pr_labels():
     
     if check_pr_exists():
         return f'Pull Request {Fore.BLUE}#{PR_NUMBER}{Fore.RESET} does not exist.'
-
-    response = requests.get(
-        f'https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/issues/{PR_NUMBER}'
-    )
     
-    if response.status_code == 200:
-        pr_data = response.json()
-        labels = pr_data.get('labels', [])
+    try:
+        response = requests.get(
+            f'https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/issues/{PR_NUMBER}'
+        )
         
-        if labels:
-            label_names = {label['name'] for label in labels}
-            relevant_labels = label_names - IGNORE_LABELS
+        if response.status_code == 200:
+            pr_data = response.json()
+            labels = pr_data.get('labels', [])
             
-            if relevant_labels:
-                if 'Needs-Author-Feedback' in relevant_labels:
-                    notification.notify(
-                        title='winget tools',
-                        message=f'pull request seems to require the author to resolve the error.',
-                        timeout=5
-                    )
-                    return f'\n{Fore.BLUE}[INFO({Fore.YELLOW}rules{Fore.BLUE})]{Fore.RESET} PR {Fore.BLUE}#{PR_NUMBER}{Fore.RESET} has the following relevant labels: {Fore.BLUE}{", ".join(relevant_labels)}{Fore.RESET}\n{Fore.BLUE}[INFO]{Fore.RESET} This pull request seems to {Fore.YELLOW}require the author to resolve the error{Fore.RESET}.'
-                elif any('Internal-Error' in label for label in relevant_labels):
-                    return f'\n{Fore.BLUE}[INFO({Fore.YELLOW}rules{Fore.BLUE})]{Fore.RESET} PR {Fore.BLUE}#{PR_NUMBER}{Fore.RESET} has the following relevant labels: {Fore.BLUE}{", ".join(relevant_labels)}{Fore.RESET}\n{Fore.BLUE}[INFO]{Fore.RESET} This pull request appears to have encountered an {Fore.YELLOW}internal error{Fore.RESET}.'
-                elif 'Moderator-Approved' in relevant_labels: # 优先级高于 Azure-Pipeline-Passed / Validation-Completed
-                    return f'\n{Fore.BLUE}[INFO({Fore.YELLOW}rules{Fore.BLUE})]{Fore.RESET} PR {Fore.BLUE}#{PR_NUMBER}{Fore.RESET} has the following relevant labels: {Fore.BLUE}{", ".join(relevant_labels)}{Fore.RESET}\n{Fore.BLUE}[INFO]{Fore.RESET} 🎉 This pull request appears to have been {Fore.GREEN}approved{Fore.RESET} by the moderator.'
-                elif 'Validation-Completed' in relevant_labels: # 优先级高于 Azure-Pipeline-Passed
-                    return f'\n{Fore.BLUE}[INFO({Fore.YELLOW}rules{Fore.BLUE})]{Fore.RESET} PR {Fore.BLUE}#{PR_NUMBER}{Fore.RESET} has the following relevant labels: {Fore.BLUE}{", ".join(relevant_labels)}{Fore.RESET}\n{Fore.BLUE}[INFO]{Fore.RESET} 🎉 Validation of this pull request appears to have been {Fore.GREEN}completed{Fore.RESET}.'
-                return f'\n{Fore.BLUE}[INFO]{Fore.RESET} PR {Fore.BLUE}#{PR_NUMBER}{Fore.RESET} has the following relevant labels: {Fore.BLUE}{", ".join(relevant_labels)}{Fore.RESET}\n{Fore.YELLOW}⚠{Fore.RESET} No rules are matched.'
+            if labels:
+                label_names = {label['name'] for label in labels}
+                relevant_labels = label_names - IGNORE_LABELS
+                
+                if relevant_labels:
+                    if 'Needs-Author-Feedback' in relevant_labels:
+                        notification.notify(
+                            title='winget tools',
+                            message=f'pull request seems to require the author to resolve the error.',
+                            timeout=5
+                        )
+                        return f'\n{Fore.BLUE}[INFO({Fore.YELLOW}rules{Fore.BLUE})]{Fore.RESET} PR {Fore.BLUE}#{PR_NUMBER}{Fore.RESET} has the following relevant labels: {Fore.BLUE}{", ".join(relevant_labels)}{Fore.RESET}\n{Fore.BLUE}[INFO]{Fore.RESET} This pull request seems to {Fore.YELLOW}require the author to resolve the error{Fore.RESET}.'
+                    elif any('Internal-Error' in label for label in relevant_labels):
+                        return f'\n{Fore.BLUE}[INFO({Fore.YELLOW}rules{Fore.BLUE})]{Fore.RESET} PR {Fore.BLUE}#{PR_NUMBER}{Fore.RESET} has the following relevant labels: {Fore.BLUE}{", ".join(relevant_labels)}{Fore.RESET}\n{Fore.BLUE}[INFO]{Fore.RESET} This pull request appears to have encountered an {Fore.YELLOW}internal error{Fore.RESET}.'
+                    elif 'Moderator-Approved' in relevant_labels: # 优先级高于 Azure-Pipeline-Passed / Validation-Completed
+                        return f'\n{Fore.BLUE}[INFO({Fore.YELLOW}rules{Fore.BLUE})]{Fore.RESET} PR {Fore.BLUE}#{PR_NUMBER}{Fore.RESET} has the following relevant labels: {Fore.BLUE}{", ".join(relevant_labels)}{Fore.RESET}\n{Fore.BLUE}[INFO]{Fore.RESET} 🎉 This pull request appears to have been {Fore.GREEN}approved{Fore.RESET} by the moderator.'
+                    elif 'Validation-Completed' in relevant_labels: # 优先级高于 Azure-Pipeline-Passed
+                        return f'\n{Fore.BLUE}[INFO({Fore.YELLOW}rules{Fore.BLUE})]{Fore.RESET} PR {Fore.BLUE}#{PR_NUMBER}{Fore.RESET} has the following relevant labels: {Fore.BLUE}{", ".join(relevant_labels)}{Fore.RESET}\n{Fore.BLUE}[INFO]{Fore.RESET} 🎉 Validation of this pull request appears to have been {Fore.GREEN}completed{Fore.RESET}.'
+                    return f'\n{Fore.BLUE}[INFO]{Fore.RESET} PR {Fore.BLUE}#{PR_NUMBER}{Fore.RESET} has the following relevant labels: {Fore.BLUE}{", ".join(relevant_labels)}{Fore.RESET}\n{Fore.YELLOW}⚠{Fore.RESET} No rules are matched.'
+                else:
+                    return f'\n{Fore.YELLOW}⚠{Fore.RESET} PR {Fore.BLUE}#{PR_NUMBER}{Fore.RESET} has labels, but all are {Fore.YELLOW}ignored{Fore.RESET}.'
             else:
-                return f'\n{Fore.YELLOW}⚠{Fore.RESET} PR {Fore.BLUE}#{PR_NUMBER}{Fore.RESET} has labels, but all are {Fore.YELLOW}ignored{Fore.RESET}.'
-        else:
-            return f'\n{Fore.BLUE}[INFO]{Fore.RESET} PR {Fore.BLUE}#{PR_NUMBER}{Fore.RESET} has {Fore.BLUE}no labels{Fore.RESET}.'
-    else:
-        return f'\n{Fore.RED}✕{Fore.RESET} Failed to retrieve PR data: {Fore.YELLOW}{response.status_code}{Fore.RESET}'
+                return f'\n{Fore.BLUE}[INFO]{Fore.RESET} PR {Fore.BLUE}#{PR_NUMBER}{Fore.RESET} has {Fore.BLUE}no labels{Fore.RESET}.'
+    except Exception as e:
+        return f'\n{Fore.RED}✕{Fore.RESET} Failed to retrieve PR data: {Fore.YELLOW}{response.status_code}{Fore.RESET}\n{Fore.RED}✕ {e}{Fore.RESET}'
 
 def wait_for_labels():
     global time_counter
