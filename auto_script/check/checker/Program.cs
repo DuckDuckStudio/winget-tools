5e43ef43819383fb8a268c71bb2b1dec9e20a738
鸭鸭「カモ」 <89643991+DuckDuckStudio@users.noreply.github.com>
2025-02-12 20:22:39 +0800

    [Auto] 自动忽略移除 - httpsacessosfiorillicombrapiinstalacaowebextensionexe

--- auto_script/check/checker/Program.cs
+++ auto_script/check/checker/Program.cs
@@ -163,7 +163,7 @@ namespace checker
         {
             HashSet<string> excludedDomains =
             [
-                "123", "360", "effie", "typora", "tchspt", "mysql", "voicecloud", "iflyrec", "jisupdf", "floorp", "https://pot.pylogmon", "https://acessos.fiorilli.com.br/api/instalacao/webextension.exe", // 之前忽略的
+                "123", "360", "effie", "typora", "tchspt", "mysql", "voicecloud", "iflyrec", "jisupdf", "floorp", "https://pot.pylogmon", // 之前忽略的
                 "https://www.betterbird.eu/", // 过于复杂
                 "https://github.com/ClassicOldSong/Apollo/releases/download/v0.2.9-alpha.3/Apollo.exe", "https://dtapp-pub.dingtalk.com/dingtalk-desktop/win_installer/Release/DingTalk_v7.6.15.91110808.exe", "https://github.com/DockerBeam/DockerBeam/releases/download/v0.1.0/DockerBeam_windows_x86.exe", // WIP
                 "https://github.com/paintdotnet/release/", // 更新时移除
