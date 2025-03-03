a49d9458c65e6062da8b2eab12f3a736f2d269e6
鸭鸭「カモ」 <89643991+DuckDuckStudio@users.noreply.github.com>
2025-03-03 13:57:04 +0800

    [Auto] 自动忽略移除 - httpsresourceslaihuacomlvpackagelaihuavideo_setup_111exe

--- auto_script/check/checker/Program.cs
+++ auto_script/check/checker/Program.cs
@@ -181,7 +181,6 @@ namespace checker
                 "https://www.deezer.com/", // 无法验证
                 "https://b2.zczc.men/file/dos-electron-assets/release/", "https://sf1-cdn-tos.douyinstatic.com/", "https://download.hellofont.cn/Client/Release/channels_backup/4.1.1/official/", "https://alistatic.lanhuapp.com/Axure/", // 不完整的url
                 "https://sourceforge.net/", // 假403
-                "https://resources.laihua.com/lvpackage/laihuavideo_setup_1.1.1.exe", // 可能为临时移除
             ];
             return excludedDomains.Any(domain => url.Contains(domain));
         }
