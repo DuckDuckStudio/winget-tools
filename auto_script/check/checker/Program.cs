dcef5d27bce329a56e1e700d7c3f2f0c934fe9f5
鸭鸭「カモ」 <89643991+DuckDuckStudio@users.noreply.github.com>
2025-03-13 09:02:07 +0800

    [Auto] 自动忽略追加 - httpsdljisutodocomjisutodo_setup_3002exe (#129)

--- auto_script/check/checker/Program.cs
+++ auto_script/check/checker/Program.cs
@@ -181,6 +181,7 @@ namespace checker
                 "https://www.deezer.com/", // 无法验证
                 "https://b2.zczc.men/file/dos-electron-assets/release/", "https://sf1-cdn-tos.douyinstatic.com/", "https://download.hellofont.cn/Client/Release/channels_backup/4.1.1/official/", "https://alistatic.lanhuapp.com/Axure/", // 不完整的url
                 "https://sourceforge.net/", // 假403
+                "https://dl.jisutodo.com/jisutodo_setup_3.0.0.2.exe", // WIP
             ];
             return excludedDomains.Any(domain => url.Contains(domain));
         }
