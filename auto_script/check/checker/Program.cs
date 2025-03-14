f1f9f1a4dcaade9815d81a8441435903f268a545
鸭鸭「カモ」 <89643991+DuckDuckStudio@users.noreply.github.com>
2025-03-14 20:20:31 +0800

    pref: 10 秒超时
    原先是 100 秒超时，这卡了一堆

--- auto_script/check/checker/Program.cs
+++ auto_script/check/checker/Program.cs
@@ -38,6 +38,7 @@ namespace checker
         {
             using HttpClient client = new();
             client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3");
+            client.Timeout = TimeSpan.FromSeconds(10);
 
             foreach (string filePath in Directory.EnumerateFiles(folderPath, "*.yaml", SearchOption.AllDirectories))
             {
