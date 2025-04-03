4f47578350898827e5c64616b14347af29db75aa
鸭鸭「カモ」 <89643991+DuckDuckStudio@users.noreply.github.com>
2025-04-03 13:29:39 +0800

    [Auto] 自动忽略追加 - httpsopenrainbowcomdownloadsRainbow_Installer_User_Electron_v213920exe

--- auto_script/check/checker/Program.cs
+++ auto_script/check/checker/Program.cs
@@ -296,6 +296,7 @@ namespace checker
                 "https://www.argyllcms.com/", // 服务器拒绝冲泡咖啡
                 "https://www.elcomsoft.com/", "https://pbank.bankcomm.cn/personbank/download/SecEditCFCAforBoCom.exe", // SSL错误
                 "https://catsxp.oss-cn-hongkong.aliyuncs.com", // 无任何可用版本
+                "https://openrainbow.com/downloads/Rainbow_Installer_User_Electron_v2.139.2.0.exe", // WIP
             ];
             return excludedDomains.Any(domain => url.Contains(domain));
         }
