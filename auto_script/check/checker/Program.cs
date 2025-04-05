550360014cf88d51e3f548ed4265967d3a03b7a4
鸭鸭「カモ」 <89643991+DuckDuckStudio@users.noreply.github.com>
2025-04-05 19:11:06 +0800

    [Auto] 自动忽略移除 - httpsota-downloadsnvidiacomotaGeForceNOW (#206)

--- auto_script/check/checker/Program.cs
+++ auto_script/check/checker/Program.cs
@@ -299,7 +299,6 @@ namespace checker
                 "https://www.elcomsoft.com/", "https://pbank.bankcomm.cn/personbank/download/SecEditCFCAforBoCom.exe", // SSL错误
                 "https://catsxp.oss-cn-hongkong.aliyuncs.com", // 无任何可用版本
                 "https://openrainbow.com/downloads/Rainbow_Installer_User_Electron_v2.139.2.0.exe", "https://openrainbow.com/downloads/", // WIP
-                "https://ota-downloads.nvidia.com/ota/GeForceNOW", // In Issue
             ];
             return excludedDomains.Any(domain => url.Contains(domain));
         }
