d620ca2ded26425fc0a81ca76e913ca906dca12b
鸭鸭「カモ」 <89643991+DuckDuckStudio@users.noreply.github.com>
2025-04-21 17:04:36 +0800

    [Auto] 自动忽略移除 - httpswwwgtopalacomdownload

--- auto_script/check/checker/Program.cs
+++ auto_script/check/checker/Program.cs
@@ -301,7 +301,6 @@ namespace checker
                 "https://www.argyllcms.com/", // 服务器拒绝冲泡咖啡
                 "https://www.elcomsoft.com/", "https://pbank.bankcomm.cn/personbank/download/SecEditCFCAforBoCom.exe", // SSL错误
                 "https://catsxp.oss-cn-hongkong.aliyuncs.com", // 无任何可用版本
-                "https://www.gtopala.com/download", // WIP
             ];
             return excludedDomains.Any(domain => url.Contains(domain));
         }
