1768bdb9ff8f41d2a26e7e34be55e5fa292796b1
鸭鸭「カモ」 <89643991+DuckDuckStudio@users.noreply.github.com>
2025-05-05 23:37:22 +0800

    fix: 以 &download 或 download 结尾的、在安装程序清单中的 URL 都视为安装程序 URL

--- checker/Program.cs
+++ checker/Program.cs
@@ -52,7 +52,7 @@ namespace checker
             }
         }
 
-        internal static readonly string[] installerType = [".exe", ".zip", ".msi", ".msix", ".appx", "&download"];
+        internal static readonly string[] installerType = [".exe", ".zip", ".msi", ".msix", ".appx", "download"];
         // &download 为 sourceforge 和类似网站的下载链接
 
         static async Task<bool> CheckUrlsInYamlFiles(string folderPath, string failureLevel)
