527ac7dbcb588f8b344e7f8444bd470f622b110b
鸭鸭「カモ」 <89643991+DuckDuckStudio@users.noreply.github.com>
2025-05-13 19:50:17 +0800

    feat: 自动重开拉取请求来重新尝试验证管道

---
+++ Retryer/Methods/Print.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Retryer.Methods
+{
+    internal class Print
+    {
+        private static bool NeedsColor()
+        {
+            if (!string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("NEEDS_COLOR")))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // 正常输出直接
+        // Console.WriteLine("");
+
+        public static void PrintInfo(string message)
+        {
+            if (NeedsColor())
+            {
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+            Console.WriteLine($"[INFO] {message}");
+            if (NeedsColor())
+            {
+                Console.ResetColor();
+            }
+        }
+
+        public static void PrintWarning(string message)
+        {
+            if (NeedsColor())
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+            Console.WriteLine($"[WARNING] {message}");
+            if (NeedsColor())
+            {
+                Console.ResetColor();
+            }
+        }
+
+        public static void PrintError(string message)
+        {
+            if (NeedsColor())
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            Console.WriteLine($"[ERROR] {message}");
+            if (NeedsColor())
+            {
+                Console.ResetColor();
+            }
+        }
+
+        public static void PrintDebug(string message)
+        {
+#if DEBUG
+            if (NeedsColor())
+            {
+                Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            }
+            Console.WriteLine($"[Debug] {message}");
+            if (NeedsColor())
+            {
+                Console.ResetColor();
+            }
+#endif
+        }
+
+        public static void PrintHint(string message)
+        {
+            if (NeedsColor())
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+            }
+            Console.WriteLine($"[Hint] {message}");
+            if (NeedsColor())
+            {
+                Console.ResetColor();
+            }
+        }
+    }
+}
