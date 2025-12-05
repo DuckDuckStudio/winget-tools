126fade718848e3648f90f157e18c7698c0674fb
鸭鸭「カモ」 <89643991+DuckDuckStudio@users.noreply.github.com>
2025-12-06 01:10:27 +0800

    feat(checker): 添加 5E.5EClient 为常失败包

--- checker/Program.cs
+++ checker/Program.cs
@@ -236,14 +236,17 @@ namespace checker
             Dictionary<string, string> FrequentlyFailingPackages = new()
             {
                 ["calibre.calibre.portable"] = "有个笨蛋经常使用 GitHub Release 的链接，GitHub Release 的链接只保留最新版本的文件，应改为 https://download.calibre-ebook.com/x.y.z/calibre-portable-installer-x.y.z.exe",
-                ["7S2P.Effie.CN"] = "包发布者经常移除此包的旧版本",
-                ["AppByTroye.KoodoReader"] = "在一段时间后，发布者会删除旧版本",
                 ["HydrusNetwork.HydrusNetwork"] = "如果该版本在发布后重新发布，则会修改版本号为 v几几几a。一般有位社区贡献者会抓住这个新版本，但TA似乎总是忘记移除旧版本。",
                 ["LutzRoeder.Netron"] = "此包的更新由作者本人维护，但他似乎只在 GitHub Release 上保留极少数版本，并且没有从 WinGet 中移除这些 404 的版本。",
-                // GeoGebra
+                // 在一段时间后，发布者会删除旧版本
+                ["AppByTroye.KoodoReader"] = "在一段时间后，发布者会删除旧版本",
+                // --------
+                ["5E.5EClient"] = "在一段时间后，发布者会删除多个旧版本",
                 ["GeoGebra.GraphingCalculator"] = "在一段时间后，发布者会删除多个旧版本",
                 ["GeoGebra.Classic"] = "在一段时间后，发布者会删除多个旧版本",
                 ["GeoGebra.Geometry"] = "在一段时间后，发布者会删除多个旧版本",
+                // --------
+                ["7S2P.Effie.CN"] = "包发布者经常移除此包的旧版本",
                 // ========
             };
             if (FrequentlyFailingPackages.TryGetValue(GetPackageIdentifier(filePath), out string? hint))
